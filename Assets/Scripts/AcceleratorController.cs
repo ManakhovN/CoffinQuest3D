0210da21d31ddde1ad591314f936af424e875dc4
Надир <nadirmanakhov@gmail.com>
2018-11-21 16:13:24 +0300

    change acceleration algorithm

--- Assets/Scripts/AcceleratorController.cs
+++ Assets/Scripts/AcceleratorController.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 using UnityEngine.UI;
 public class AcceleratorController : MonoBehaviour {
-    public float timeForShake = 0.2f;
+/*    public float timeForShake = 0.2f;
     float delta;
     float delay=0.1f;
     Vector3 previousAcceleration;
@@ -37,5 +37,48 @@ public class AcceleratorController : MonoBehaviour {
     public void reset()
     {
         delta = timeForShake;
+    }*/
+
+    float accelerometerUpdateInterval = 1.0f / 60.0f;
+    // The greater the value of LowPassKernelWidthInSeconds, the slower the
+    // filtered value will converge towards current input sample (and vice versa).
+    public float lowPassKernelWidthInSeconds = 1.0f;
+    // This next parameter is initialized to 2.0 per Apple's recommendation,
+    // or at least according to Brady! ;)
+    public float shakeDetectionThreshold = 2.0f;
+
+    float lowPassFilterFactor;
+    Vector3 lowPassValue;
+
+    void Start()
+    {
+        lowPassFilterFactor = accelerometerUpdateInterval / lowPassKernelWidthInSeconds;
+        shakeDetectionThreshold *= shakeDetectionThreshold;
+        lowPassValue = Input.acceleration;
+    }
+    Vector3 deltaAcceleration;
+    void Update()
+    {
+        Vector3 acceleration = Input.acceleration;
+        lowPassValue = Vector3.Lerp(lowPassValue, acceleration, lowPassFilterFactor);
+        deltaAcceleration = acceleration - lowPassValue;
+
+        if (deltaAcceleration.sqrMagnitude >= shakeDetectionThreshold)
+        {
+            // Perform your "shaking actions" here. If necessary, add suitable
+            // guards in the if check above to avoid redundant handling during
+            // the same shake (e.g. a minimum refractory period).
+            Debug.Log("Shake event detected at time " + Time.time);
+        }
+    }
+
+    public bool isShaked()
+    {
+        return deltaAcceleration.sqrMagnitude >= shakeDetectionThreshold;
+    }
+
+    public void reset()
+    {
+        deltaAcceleration = Vector3.zero;
     }
 }
---
+++ Assets/Scripts/SettingsInputController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsInputController : MonoBehaviour {
+    float timer = 0;
+    bool mouseDown = false;
+    public GameObject settingsPanel;
+    AcceleratorController acceleratorController;
+    Slider lowPassSlider;
+    Slider treshHoldSlider;
+    Text treshHoldLabel;
+    Text lowPassLabel;
+    public void Awake()
+    {
+        acceleratorController = FindObjectOfType<AcceleratorController>();
+
+        lowPassSlider = settingsPanel.transform.Find("lowPassKernelSlider").GetComponent<Slider>();
+        treshHoldSlider = settingsPanel.transform.Find("shakeDetectionThresholdSlider").GetComponent<Slider>();
+        treshHoldLabel = settingsPanel.transform.Find("shakeDetectionThresholdLabel").GetComponent<Text>();
+        lowPassLabel = settingsPanel.transform.Find("lowPassKernelLabel").GetComponent<Text>();
+        if (!PlayerPrefs.HasKey("lowPassKernel"))
+            PlayerPrefs.SetFloat("lowPassKernel", acceleratorController.lowPassKernelWidthInSeconds);
+
+        lowPassChanged(PlayerPrefs.GetFloat("lowPassKernel"));
+        lowPassSlider.onValueChanged.AddListener(lowPassChanged);
+
+        if (!PlayerPrefs.HasKey("shakeDetectionThreshold"))
+            PlayerPrefs.SetFloat("shakeDetectionThreshold", acceleratorController.shakeDetectionThreshold);
+        treshHoldChanged(PlayerPrefs.GetFloat("shakeDetectionThreshold"));
+        treshHoldSlider.onValueChanged.AddListener(treshHoldChanged);
+    }
+
+    private void treshHoldChanged(float arg0)
+    {
+        PlayerPrefs.SetFloat("shakeDetectionThreshold", arg0);
+        PlayerPrefs.Save();
+        acceleratorController.shakeDetectionThreshold = PlayerPrefs.GetFloat("shakeDetectionThreshold");
+        treshHoldLabel.text = "treshHold = " + PlayerPrefs.GetFloat("shakeDetectionThreshold");
+        treshHoldSlider.value = PlayerPrefs.GetFloat("shakeDetectionThreshold");
+    }
+
+    private void lowPassChanged(float arg0)
+    {
+        PlayerPrefs.SetFloat("lowPassKernel",arg0);
+        PlayerPrefs.Save();
+        acceleratorController.lowPassKernelWidthInSeconds = PlayerPrefs.GetFloat("lowPassKernel");
+        lowPassSlider.value = PlayerPrefs.GetFloat("lowPassKernel");
+        lowPassLabel.text = "lowPass = " + PlayerPrefs.GetFloat("lowPassKernel");
+    }
+
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+            mouseDown = true;
+        if (Input.GetMouseButtonUp(0))
+            mouseDown = false;
+
+        if (mouseDown)
+            timer += Time.deltaTime;
+        else
+            timer = 0f;
+
+        if (timer > 20f)
+        {
+            settingsPanel.SetActive(true);
+            timer = 0f;
+        }
+	}
+}
